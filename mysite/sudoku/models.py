331126248eaf9b2e2b48058eaa5fac42057c14b1
zlima410 <endercraft68@gmail.com>
2024-03-05 19:49:53 -0500

    Create model for the sudoku puzzle with a puzzle and its solution

--- mysite/sudoku/models.py
+++ mysite/sudoku/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Sudoku(models.Model):
+    puzzle = models.CharField(max_length=81)
+    solution = models.CharField(max_length=81)
+
+    def __str__(self):
+        return f"Sudoku Puzzle: {self.pk}"
