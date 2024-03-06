2b96a40571a98567816801cdacc160ce9a484310
zlima410 <endercraft68@gmail.com>
2024-03-05 19:57:14 -0500

    Create solve_sudoku function and complete is_valid function to check if a number is in the correct place

--- mysite/sudoku/views.py
+++ mysite/sudoku/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the sudoku index.")
+def solve_sudoku(board):
+    # check if a number is valid in a cell
+    def is_valid(row, col, num):
+        # check the row and the column
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
+                return False
+            
+        # check 3x3 subgrid
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if board[start_row + i][start_col + j] == num:
+                    return False
+        return True
+    
+    
