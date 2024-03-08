843a7f02954cb5b27970416e3b5010a747dcb89b
zlima410 <endercraft68@gmail.com>
2024-03-07 21:53:56 -0500

    Update time zone and fix URL path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("/sudoku/", include("sudoku.urls")),
-    path('/admin/', admin.site.urls),
+    path("sudoku/", include("sudoku.urls")),
+    path('admin/', admin.site.urls),
 ]
