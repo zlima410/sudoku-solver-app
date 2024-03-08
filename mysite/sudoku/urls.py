64483003ede248d49ba13ee4e03c4dc9a7444e27
zlima410 <endercraft68@gmail.com>
2024-03-07 21:42:53 -0500

    Update sudoku URLs to include generate and solve puzzle views

--- mysite/sudoku/urls.py
+++ mysite/sudoku/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import generate_puzzle, solve_puzzle
 
 urlpatterns = [
-    
+    path('/generate/', generate_puzzle, name='generate_puzzle'),
+    path('/solve/<int:puzzle_id>/', solve_puzzle, name='solve_puzzle'),
 ]
