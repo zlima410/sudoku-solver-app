1e5ece96c2346b357e401664624b89ed68efb687
zlima410 <endercraft68@gmail.com>
2024-03-07 21:47:26 -0500

    Update project settings and URLs

--- mysite/sudoku/models.py
+++ mysite/sudoku/models.py
@@ -6,5 +6,5 @@ class Sudoku(models.Model):
     puzzle = models.CharField(max_length=81)
     solution = models.CharField(max_length=81)
 
-    def __str__(self):
-        return f"Sudoku Puzzle: {self.pk}"
+    """def __str__(self):
+        return f"Sudoku Puzzle: {self.pk}" """
--- mysite/sudoku/urls.py
+++ mysite/sudoku/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import generate_puzzle, solve_puzzle
 
 urlpatterns = [
-    path('/generate/', generate_puzzle, name='generate_puzzle'),
-    path('/solve/<int:puzzle_id>/', solve_puzzle, name='solve_puzzle'),
+    path('generate/', generate_puzzle, name='generate_puzzle'),
+    path('solve/<int:puzzle_id>/', solve_puzzle, name='solve_puzzle'),
 ]
